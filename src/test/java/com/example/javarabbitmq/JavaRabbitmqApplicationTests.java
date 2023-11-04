4c4bb4bf961eef25dc6a946f7e9538990e5ea344
yushin <kkad45@naver.com>
2023-11-04 15:00:46 +0900

    init

---
+++ src/test/java/com/example/javarabbitmq/JavaRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javarabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
